2f92e0eeddb686c35194abbab0bd14934d4e9468
Shijun Zhang <sjzhang.math@outlook.com>
2020-03-07 22:19:46 +0800

    adapt to phone version

--- file/cssAndJs/script.js
+++ file/cssAndJs/script.js
@@ -7,18 +7,35 @@ dddd<span id="zsj"></span>dddd
     navBarItem.innerHTML="zsjzsj"	
 </script>*/
 // alert(screen.width)
-var	w0=screen.width;
-var	h0=screen.height;
+
+//screen.width window.outerWidth return width  in terms of point  1 pt=1/72 inch
+// screen.innerWidth returns width in point for PCs
+// screen.innerWidth returns width in css px for cellphones
+var	w0=window.screen.width;
+var	h0=window.screen.height;
 w=Math.max(w0,h0);
 h=w0+h0-w;
 var navBarFontSize=20;
 var footerFontSize=20;
 var phoneOrPc=0;
-if (window.innerWidth<=window.innerHeight && Math.abs(window.orientation-90)==90) {
-	w=window.innerWidth;
-	h=window.innerHeight;
-	phoneOrPc=1;
-}
+//window.innerWidth //width of browser
+// window.orientation is undefined in pc, use window.screen.orientation.angle instead
+// window.screen.orientation.angle=0 usually for pc
+
+// verical phone
+if (window.innerHeight>=window.innerWidth && Math.abs(window.screen.orientation.angle-90)==90) {
+	h=Math.max(window.innerWidth,window.innerHeight);
+	w=window.innerWidth+window.innerHeight-h;
+	phoneOrPc=1;	
+} 
+//alert(window.innerWidth+'s'+window.innerHeight+'s'+window.screen.orientation.angle+'s'+phoneOrPc)
+
+// horizontal phone, it is regarded as a PC
+if (window.innerHeight<=window.innerWidth && Math.abs(window.screen.orientation.angle)==90) {
+	h=Math.min(window.innerWidth,window.innerHeight);
+	w=window.innerWidth+window.innerHeight-h;
+	phoneOrPc=0;	
+} 
 
 // if (window.orientation == 180 || window.orientation == 0) {
 //       alert('竖屏')
@@ -33,14 +50,14 @@ var whitSpaceForNavBar=document.getElementById("whitSpaceForNavBar");
 whitSpaceForNavBar.style.height=window.getComputedStyle(navBar,null).getPropertyValue("height");
 //set navBar fontsize
 
-var narVavBarFontSize=0.018*w;
-if (phoneOrPc==1) {narVavBarFontSize=0.028*w;}
+var narVavBarFontSize=0.02*w;
+if (phoneOrPc==1) {narVavBarFontSize=0.0342*w;}
 
 navBar.style.setProperty("font-size",narVavBarFontSize+"px");
 navBar.style.setProperty("font-weight",540);
-var leftMostNavBarItem=document.getElementById("leftMostNavBarItem")
-if (phoneOrPc==0){leftMostNavBarItem.style.width=w*0.0655421+"px"}
-
+var leftMostNavBarItemWidth=w*0.0655421;
+if (phoneOrPc==1){leftMostNavBarItemWidth=w*0.032}
+document.getElementById("leftMostNavBarItem").style.width=leftMostNavBarItemWidth+"px";
 
 
 
@@ -81,17 +98,22 @@ if (phoneOrPc==0) {var h1Size=w*0.024; pageWidth=0.8*w;} else {  var h1Size=w*0.
 var insertStyle=new Array()
 insertStyle[0]=" h1 { font-size:"+h1Size+"px;color:black;text-align:center;font-weight:650;}";
 insertStyle[1]=" h2 { font-size:"+h1Size*0.9+"px;color:black;text-align:left;font-weight:545;}";
-insertStyle[2]=" h3 { font-size:"+h1Size*0.81+"px;color:black;text-align:left;font-weight:450;}";
-insertStyle[3]=" .normalFont {font-size:"+h1Size*0.729+"px;color:#555555;text-align:left;line-height:"+h1Size*0.999  +"px;}";
+insertStyle[2]=" h3 { font-size:"+h1Size*0.8+"px;color:black;text-align:left;font-weight:450;}";
+insertStyle[3]=" .normalFont {font-size:"+h1Size*0.7+"px;color:#555555;text-align:left;line-height:"+h1Size*0.999  +"px;}";
 insertStyle[4]=" .pageContainer { display:block;margin:0 auto;width:"+pageWidth+"px;}";
 insertStyle[5]=" .addWhiteSpaceOne { height:"+h1Size+"px;}";
-insertStyle[6]=" .addWhiteSpaceTwo { height:"+h1Size*0.8 +"px;}";
-insertStyle[7]=" .addWhiteSpaceThree { height:"+h1Size*0.6+"px;}";
+insertStyle[6]=" .addWhiteSpaceTwo { height:"+h1Size*0.7 +"px;}";
+insertStyle[7]=" .addWhiteSpaceThree { height:"+h1Size*0.4+"px;}";
+
+
 // styleInJs.innerHTML=insertStyle.join('') 
 //InsertStyle will be added in corresponding HTML file, variables are valid in whole HTML file
 
 /*alert('w'+window.innerWidth+'wc'+screen.width)*/
 
 
-/*//refresh if the window size is changed
-window.onresize =function(){location.reload()}*/
+//refresh if the window size is changed
+// window.onresize =function(){location.reload();} // it can be replaced by 
+//window.addEventListener('resize',function(){location.reload();})
+window.addEventListener("orientationchange",function (){location.reload();})
+// reload the page if its orientation is changed
