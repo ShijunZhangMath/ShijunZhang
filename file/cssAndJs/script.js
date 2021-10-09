86ea150e07e4cc0b062961e2e9d3cfb28ea1d3ea
Shijun Zhang <sjzhang.math@outlook.com>
2021-10-09 08:35:30 +0800

    update js

--- file/cssAndJs/script.js
+++ file/cssAndJs/script.js
@@ -16,7 +16,7 @@ dddd<span id="zsj"></span>dddd
 //screen.width window.outerWidth return width  in terms of point  1 pt=1/72 inch
 // screen.innerWidth returns width in point for PCs
 // screen.innerWidth returns width in css px for cellphones
-var dayUpdate="October 8, 2021"
+var dayUpdate="October 9, 2021"
 
 
 var	w=window.innerWidth;
@@ -76,15 +76,6 @@ if (window.innerHeight<=window.innerWidth && Math.abs(myorientation)==90) {
 
 
 
-
-
-
-
-
-
-
-
-
 // define a function to test pathname
 function testPN(pn) {
 var currentPN=document.location.pathname;
@@ -99,6 +90,10 @@ var moreOrNot=testPN("/more");
 var interestOrNot=testPN("/interest");
 var homeOrNot=1-cvOrNot-publicationOrNot-moreOrNot-interestOrNot;
 // return 1 if home; return 0 if not
+
+
+var relativePath='../';
+if (homeOrNot){ relativePath='./';}
 
 
 /*add links or metas to head*/
@@ -118,14 +113,23 @@ linkNewApple.href=iconPN;
 document.getElementsByTagName("head")[0].appendChild(linkNewApple);
 
 
+// add icon library
+var iconLibrary = document.createElement("link");
+iconLibrary.rel = "stylesheet"; 
+iconLibrary.href=relativePath+"file/icon/academicons/css/academicons.min.css";
+document.getElementsByTagName("head")[0].appendChild(iconLibrary);
+
+/*var iconLibrary = document.createElement("link");
+iconLibrary.rel = "stylesheet"; 
+iconLibrary.href=relativePath+"file/icon/zsjIcons.css";
+document.getElementsByTagName("head")[0].appendChild(iconLibrary);*/
+
+
 
 
 // Add HTML to nav bar
 var navBarHTML=document.getElementById("navBarHTML");
 var myNavBarHTML = new Array();
-
-var relativePath='../';
-if (homeOrNot){ relativePath='./';}
 
 var homeActiveOrNot=" ";
 if (homeOrNot){
@@ -153,16 +157,16 @@ if (moreOrNot){
 }
 
 
-myNavBarHTML[0]='<nav id="navBar">      <ul class="navBarContainer navBarLinkColor" id="navBarInner">';
+myNavBarHTML[0]='<nav id="navBar">'+
+						'<ul class="navBarContainer navBarLinkColor" id="navBarInner">';
 
+myNavBarHTML[1]='<li class="navBarItem" id="navBarHome">'+
+	'<a href="'+relativePath+'" class="myTextShadow '+homeActiveOrNot+'"> Shijun&thinsp;ZHANG </a>'+
+		'</li>';
 
-var homeName='<a href="'+relativePath+'" class="myTextShadow '+homeActiveOrNot+'"> Shijun&thinsp;ZHANG </a>';
-
-myNavBarHTML[1]='<li class="navBarItem" id="navBarHome">'+homeName+'</li>';
-
-
-myNavBarHTML[2]='<li class="navBarItem"><a href="'+relativePath+'publication/" class="'+publicationActiveOrNot+'"> Publication </a>  </li>';
-
+myNavBarHTML[2]='<li class="navBarItem">'+
+					'<a href="'+relativePath+'publication/" class="'+publicationActiveOrNot+'"> Publication </a>'+
+				'</li>';
 
 myNavBarHTML[3]='<li class="navBarItem">  <a href="'+relativePath+'interest/" class="'+interestActiveOrNot+'"> Interest </a>  </li>';
 
@@ -183,8 +187,13 @@ Starting with ../../ moves two directories backward and starts there (and so on.
 
 To move forward, just start with the first sub directory and keep moving forward.
 */
-myNavBarHTML[6]='</ul>'+' <div id="navBarRule" style="position:fixed; top: auto; left:0px;width:100%;"><hr class="myHr"></div>  ';
-myNavBarHTML[7]='<div id="vspaceAfterNavBar"></div>            </nav>'; 
+myNavBarHTML[6]='</ul>'+
+				'<div id="navBarRule" style="position:fixed; top: auto; left:0px;width:100%;">'+
+					'<hr class="myHr">'+
+				'</div>';
+
+myNavBarHTML[7]=   '<div id="vspaceAfterNavBar"></div>' +
+				'</nav>'; 
 navBarHTML.innerHTML=myNavBarHTML.join('')  
 /*.join() has commas
 const elements = ['Fire', 'Air', 'Water'];
@@ -207,7 +216,6 @@ if (phoneOrPc) {navBarFontSize=0.03*w;}
 
 navBar.style.setProperty("font-size",navBarFontSize+"px");
 navBar.style.setProperty("font-family","Helvetica, sans-serif");
-
 
 /*navBar.style.setProperty("font-weight",440);*/
 var navBarHome=document.getElementById("navBarHome");
@@ -232,48 +240,59 @@ vspaceAfterNavBar.style.height=navBarH;
 
 
 
-
-
-
 //  add footer
-var navBarFontSize, footerFontSize=20;
+var footerFontSize=20;
 var footer=document.getElementById("footer");
 var myFooter = new Array();
 
-myFooter[0]=" <div style='background-color:rgba(240,240,240,0.95); display:block;position:sticky;'>";
+myFooter[0]=" <div style='background-color:rgba(240,240,240,0.8); display:block;position:sticky;'>";
+
 myFooter[1]="<div style='height:"+h*0.053+"px;'></div>";// add vspace
+
 myFooter[2]=" Email:&ensp;zhangshijun@u.nus.edu &ensp;<b>or</b>&ensp; shijun.math@outlook.com <br>";
-myFooter[3]="Address:&ensp;Department of Mathematics (NUS), Singapore 119076 ";
+
+myFooter[3]="Address:&ensp;Department of Mathematics (NUS), Singapore 119076";
+
 
 /*var footerImgPN="../file/img/zsjBlue.gif";
 if (homeOrNot) {var footerImgPN="./file/img/zsjBlue.gif";} 
 var footerPicWidth=w*0.018*12*2.2;
 if (phoneOrPc) {footerPicWidth=w*0.018*12*2*2.2;}
 myFooter[2]="<img src=" +  footerImgPN   +" style='width:"+ footerPicWidth +"px'>";*/
-myFooter[4]="<div id='backgroundSource'></div> Last updated on "+dayUpdate;
-myFooter[5]="<div style='height:1em;'></div>";
-myFooter[6]="<a href='"+relativePath+"'> <img src='"+relativePath+"file/img/zsjLogo2.png' style='height:3.5em;width:auto;'> </a>";
-myFooter[7]="<div style='height:2em;'></div>        </div>";
+myFooter[4]="<div id='backgroundSource'></div> Last updated on "+ dayUpdate;
+
+myFooter[5]="<div style='height:1em;'></div> ";
+
+
+myFooter[6]="<div class='footerLinks'>"+
+			" <a href='https://scholar.google.com/citations?user=NZA4ur4AAAAJ&hl=en/' title='Google Scholar'>" +
+			"<i class='ai ai-google-scholar-square'></i>    </a>";
+
+myFooter[7]="<a href='https://orcid.org/0000-0003-4115-7891/' title='ORCID'>" +
+			" <i class='ai ai-orcid-square'></i>    </a>";
+
+myFooter[8]="<a href='"+relativePath+"file/pdf/CV_ShijunZHANG.pdf' title='Curriculum Vitae'>" +
+			" <i class='ai ai-cv-square'></i>      </a>";
+/*myFooter[9]="<a href='https://www.researchgate.net/profile/Shijun-Zhang-6/'>" +
+			" <i class='ai ai-researchgate-square'></i></a>"+*/
+myFooter[9]="</div>";
+
+
+myFooter[10]="<div style='height:2em;'></div>"+
+			"</div>";
 /*myFooter[3]="<div style='height:30px'> </div>";*/
 footer.innerHTML=myFooter.join('') 
 
-
 var myfooterFontSize=w*0.01*1.8*0.75; 
-var myfooterLineHeight=myfooterFontSize*1.25;
+var myfooterLineHeight=myfooterFontSize*1.35;
 if (phoneOrPc) {
 	 myfooterFontSize=w*0.01*1.8*0.6*2.374; 
 		myfooterLineHeight=myfooterFontSize*1.45;
 	}
-
 footer.setAttribute("style","text-align:center;text-size-adjust:none;-webkit-text-size-adjust:none; ");
 footer.style.fontSize=myfooterFontSize+"px";
 footer.style.lineHeight=myfooterLineHeight+"px";
 footer.style.maxHeight= "100%";
-
-
-
-
-
 
 
 
@@ -287,18 +306,27 @@ else {
 
 var insertStyle=new Array();
 insertStyle[0]=" h1 { font-size:"+h1Size+"px;color:#242424;text-align:center;font-weight:600;font-family:serif;}";
+
 insertStyle[1]=" h2 { font-size:"+h1Size*0.9+"px;color:#242424;text-align:left;font-weight:500;font-family:sans-serif;}";
+
 insertStyle[2]=" h3 { font-size:"+h1Size*0.81+"px;color:#242424;text-align:left;font-weight:400;font-family:sans-serif;}";
+
 insertStyle[3]=" .normalFont {font-size:"+h1Size*0.729+"px;color:#242424;text-align:left;line-height:";
+
 insertStyle[4]= lineHeight*0.98  +"px;font-family:Helvetica, sans-serif;}";
+
 insertStyle[5]=" .smallFont {font-size:"+h1Size*0.729*0.9+"px;color:#242424;text-align:left;line-height:";
+
 insertStyle[6]= lineHeight*0.85  +"px;font-family:Helvetica, sans-serif;}";
+
 insertStyle[7]=" .footnoteFont {font-size:"+h1Size*0.729*0.9*0.9+"px;color:#242424;text-align:left;line-height:";
+
 insertStyle[8]= lineHeight*0.85*0.9  +"px;font-family:Helvetica, sans-serif;}";
+
 if (phoneOrPc) {
-	insertStyle[9]= ".imgWidth {width:88%;padding:"+0.019*h+"px;}";
+	insertStyle[9]= ".imgWidth {width:88%;padding-top:"+0.0328*h+"px;padding-bottom:"+0.0328*h+"px;}";
 }else{
-	insertStyle[9]= ".imgWidth {width:68%;padding:"+0.0218*h+"px;}";
+	insertStyle[9]= ".imgWidth {width:68%;padding-top:"+0.025*h+"px;padding-bottom:"+0.0328*h+"px;}";
 }
 
 /*insertStyle[9]=" .pageContainer { display:block;margin:0 auto;width:"+pageWidth+"px;}";*/ /* this is overwrite by Css in each index.html*/
---
+++ file/icon/fontisto/README.md
@@ -0,0 +1,186 @@
+<p align="center">
+  <a href="http://fontisto.com"><img src="http://fontisto.com/assets/images/fontisto-cover-1.png"></a>
+  
+  <p align="center">
+   Fontisto gives you scalable vector icons that can instantly be customized: size, color, drop shadow and anything that can be done with the power of CSS.
+    <br>
+    <a href="http://www.fontisto.com/get-started"><strong>Fontisto get-started</strong></a> &middot;
+    <a href="http://fontisto.com/examples"><strong>Fontisto example</strong></a> &middot;
+    <a href="http://fontisto.com/icons"><strong>Fontisto icons</strong></a> &middot;
+    <a href="http://fontisto.com/icons-cheatsheet"><strong>Fontisto cheatsheet</strong></a>
+  </p>
+  <p align="center">
+    <a href="https://www.npmjs.org/package/fontisto">
+      <img src="https://img.shields.io/npm/v/fontisto.svg?style=flat" alt="npm">
+    </a>
+  </p>
+</p>
+
+## Fontisto (The iconic font and CSS toolkit)
+
+Fontisto is a full suite of 616+ pictographic icons for easy scalable vector graphics on websites,
+created and maintained by [Kenan Gündoğan](https://www.linkedin.com/in/kenangundogan).
+Stay up to date with the latest release and announcements on Twitter:
+[@fontisto](http://twitter.com/fontisto).
+
+Get started at http://fontisto.com
+
+
+## Quick start
+[![Build Status](https://travis-ci.org/kenangundogan/fontisto.svg?branch=master)](https://travis-ci.org/kenangundogan/fontisto)
+[![npm version](https://badge.fury.io/js/fontisto.svg)](https://badge.fury.io/js/fontisto)
+[![PHP version](https://badge.fury.io/ph/kenangundogan%2Ffontisto.svg)](https://badge.fury.io/ph/kenangundogan%2Ffontisto)
+[![Meteor Atmosphere](https://img.shields.io/badge/meteor-kenangundogan/fontisto-blue.svg)](https://atmospherejs.com/kenangundogan/fontisto)
+
+- [Download the latest release.](https://codeload.github.com/kenangundogan/fontisto/zip/master)
+- Clone the repo: `git clone https://github.com/kenangundogan/fontisto.git`
+- Install with [npm](https://www.npmjs.com/package/fontisto): `npm install fontisto`
+- Install with [bower](https://bower.io): `bower install fontisto`
+- Install with [yarn](https://yarnpkg.com/en/package/fontisto): `yarn add fontisto`
+- Install with [composer](https://packagist.org/packages/kenangundogan/fontisto): `composer require kenangundogan/fontisto`
+- Install with [meteor](https://atmospherejs.com/kenangundogan/fontisto): `meteor add kenangundogan:fontisto`
+
+
+## What's included
+Within the download you'll find the following directories and files, logically grouping common assets and providing both compiled and minified variations. 
+
+#### Outuput File
+
+```
+	css/
+	├── fontisto/
+		├── fontisto.css
+		├── fontisto.css.map
+		├── fontisto.min.css
+		└── fontisto.min.css.map
+```
+
+#### Source File (Sass/Scss)
+```
+	sass/
+	├── fontisto/
+		├── fontisto.scss
+		├── _variables.scss
+		├── _color.scss
+		├── _font.scss
+		├── _icons.scss
+		├── _animated.scss
+		├── _list.scss 
+		├── _bordered.scss
+		├── _rotated.scss
+		├── _stacked.scss
+		├── _fixed-width.scss
+		└── _screen-readers.scss
+```
+#### Source File (Less)
+```
+	less/
+	├── fontisto/
+		├── fontisto.less
+		├── variables.less
+		├── color.less
+		├── font.less
+		├── icons.less
+		├── animated.less
+		├── list.less 
+		├── bordered.less
+		├── rotated.less
+		├── stacked.less
+		├── fixed-width.less
+		└── screen-readers.less
+```
+#### Source File (Stylus)
+```
+	stylus/
+	├── fontisto/
+		├── fontisto.styl
+		├── variables.styl
+		├── color.styl
+		├── font.styl
+		├── icons.styl
+		├── animated.styl
+		├── list.styl 
+		├── bordered.styl
+		├── rotated.styl
+		├── stacked.styl
+		├── fixed-width.styl
+		└── screen-readers.styl
+```
+
+
+## Versioning
+Fontisto will be maintained under the Semantic Versioning guidelines as much as possible. Releases will be numbered with the following format:
+```
+<major>.<minor>.<patch>
+```
+
+## License
+
+- The Fontisto font is licensed under the SIL OFL 1.1:
+  - http://scripts.sil.org/OFL
+- Fontisto CSS, LESS, and Sass files are licensed under the MIT License:
+  - https://opensource.org/licenses/mit-license.html
+- The Fontisto documentation is licensed under the CC BY 3.0 License:
+  - http://creativecommons.org/licenses/by/3.0/
+- Attribution is no longer required as of Fontisto 3.0, but much appreciated:
+  - `Fontisto by Kenan Gündoğan - http://fontisto.com`
+- Full details: http://fontisto.com/license
+
+
+## Copyright and license
+Designed and built by @kenangundogan, @ulasderin and @mstfsnc
+<br>
+2017 Currently - Code licensed [MIT License](https://github.com/kenangundogan/fontisto/blob/master/LICENSE)
+
+
+<p align="center">
+  <a href="http://fontisto.com/browser-addon"><img src="http://fontisto.com/assets/images/browser-addon-screen-shot/fontisto-browser-extension-1280x800-2.png"></a>
+  
+  <p align="center">
+   Fontisto browser addons (chrome, firefox, opera)
+    <br>
+    <a href="http://fontisto.com/browser-addon"><strong>Fontisto Browser Addons</strong></a> &middot;
+    <a href="https://chrome.google.com/webstore/detail/fontisto/gnmccfjgnhfoihcpglcogbclihomaidh?hl=en"><strong>Fontisto Chrome Extension</strong></a> &middot;
+    <a href="https://addons.mozilla.org/tr/firefox/addon/fontisto/"><strong>Fontisto Firefox Addon</strong></a> &middot;
+    <a href="https://addons.opera.com/tr/extensions/details/fontisto/"><strong>Fontisto Opera Addon</strong></a> &middot;
+  </p>
+</p>
+
+## ToDo List (font with css)
+- [x] font width css (**completed**)
+- [x] scss to css (**completed**)
+- [x] less to css (**completed**)
+- [x] stylus to css (**completed**)
+
+## ToDo List (Advanced Packaged)
+- [ ] Svg with js
+- [ ] Svg sprite
+- [ ] Vue.js
+- [ ] React
+- [ ] Angular
+
+## ToDo List (icon set)
+- [x] Brand (**completed**)
+- [x] Browser (**completed**)
+- [x] Accessibility (**completed**)
+- [x] Chart (**completed**)
+- [x] currency (**completed**)
+- [x] Directional (**completed**)
+- [x] Form Control (**completed**)
+- [x] Gender (**completed**)
+- [x] Hand (**completed**)
+- [x] Payment (**completed**)
+- [x] Player (**completed**)
+- [x] Spinner (**completed**)
+- [x] Transportation (**completed**)
+- [x] Web Application (**completed**)
+- [x] Text Editor (**completed**)
+- [x] Weather Foreces (**completed**)
+- [x] Emoji (**completed**)
+- [x] Shopping (**completed**)
+- [x] Snow Flake (**completed**)
+- [x] Travel (**completed**)
+- [ ] Sports
+- [x] Medical (**completed**)
+- [ ] Eat
+- [ ] Shapes
