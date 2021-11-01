5f684fafc7cb6736393f6c3d60ea4d29429809f7
Shijun Zhang <sjzhang.math@outlook.com>
2021-11-01 22:30:15 +0800

    two minor modifications

--- file/cssAndJs/script.js
+++ file/cssAndJs/script.js
@@ -16,7 +16,7 @@ dddd<span id="zsj"></span>dddd
 //screen.width window.outerWidth return width  in terms of point  1 pt=1/72 inch
 // screen.innerWidth returns width in point for PCs
 // screen.innerWidth returns width in css px for cellphones
-var dayUpdate="October 22, 2021";
+var dayUpdate="November 1, 2021";
 
 
 var	w=window.innerWidth;
@@ -212,7 +212,7 @@ console.log(elements.join('-'));
 //navBar setting
 var navBar=document.getElementById("navBar");
 //set navBar fontsize
-var navBarFontSize=0.0116*w;
+var navBarFontSize=0.0114*w;
 if (phoneOrPc) {navBarFontSize=0.0300*w;}
 
 navBar.style.setProperty("font-size",navBarFontSize+"px");
@@ -230,7 +230,7 @@ if (phoneOrPc){
 	navBarHome.style.setProperty("font-size",navBarFontSize*1.585+"px");
 }else{
 	navBarH=navBarFontSize*4.54+"px";
-	navBarHome.style.setProperty("padding-right","27.985%");
+	navBarHome.style.setProperty("padding-right","28.2%");
 	navBarHome.style.setProperty("font-size",navBarFontSize*1.8+"px");
 }
 
