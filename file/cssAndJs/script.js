dc31f4184dbff6befa4271a7c79094399ffb699a
Shijun Zhang <sjzhang.math@outlook.com>
2022-08-01 13:51:44 -0400

    update CV

--- file/cssAndJs/script.js
+++ file/cssAndJs/script.js
@@ -23,7 +23,7 @@ function isMobile() {
 //screen.width window.outerWidth return width  in terms of point  1 pt=1/72 inch
 // screen.innerWidth returns width in point for PCs
 // screen.innerWidth returns width in css px for cellphones
-var dayUpdate="July 28, 2022";
+var dayUpdate="August 1, 2022";
 var jsInsertStyle=" ";
 
 
